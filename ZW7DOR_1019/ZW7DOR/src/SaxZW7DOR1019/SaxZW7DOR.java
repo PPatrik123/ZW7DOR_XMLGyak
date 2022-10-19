e7a59ee057ed5d668585c932879d3c3968288286
PPatrik123 <78854607+PPatrik123@users.noreply.github.com>
2022-10-19 11:27:31 +0200

    Add files via upload

---
+++ ZW7DOR_1019/ZW7DOR/src/SaxZW7DOR1019/SaxZW7DOR.java
@@ -0,0 +1,83 @@
+package SaxZW7DOR1019;
+
+
+
+
+public class SaxZW7DOR{
+    
+    public static void main(String[] args){
+        try{
+            SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
+            
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            
+            SaxHandler handler = new SaxHandler();
+            
+            saxParser.parse(new File("kurzusfelvetell.xml"),handler);
+            
+        } catch(ParserConfigurationException | SAXException | IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+}
+
+class SaxHandler extends DefaultHandler{
+    private int indent = 0;
+
+private String formatAttributes(Attributes attributes) {
+    int attrLenght = attributes.getLength();
+    if(attrLenght == 0)
+    {
+        return "";
+    }
+    StringBuilder sb = new StringBuilder(", {");
+    for(int i = 0; i < attrLenght; i++)
+    {
+        sb.append(attributes.getLocalName(i) + "=" + attributes.getValue(i));
+        if(i<attrLenght - 1)
+        {
+            sb.append(", ");
+        }
+    }
+    sb.append("}");
+    return sb.toString();
+}
+
+private void indent() {
+    for(int i = 0; i < indent; i++)
+    {
+        System.out.print("   ");
+    }
+}
+
+@Override
+public void startElement(String uri, String localName, String qName, Attributes attributes)
+{
+    indent++;
+    indent();
+    System.out.println(qName + formatAttributes(attributes) + " start ");
+
+}
+@Override
+public void endElement(String uri, String localName, String qName)
+{
+    indent();
+    indent--;
+    System.out.println(qName + "end");
+}
+
+@Override
+public void characters(char ch[], int start, int lenght) 
+{
+    String chars = new String(ch,start,lenght).trim();
+    if(!chars.isEmpty())
+    {
+        indent++;
+        indent();
+        indent--;
+        System.out.println(chars);
+    }
+}
+
+}
